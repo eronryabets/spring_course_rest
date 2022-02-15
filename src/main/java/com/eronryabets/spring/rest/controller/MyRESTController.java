0fb85324134da28c08da344603cc6b4ca865e681
Ihor Ryabets <eronryabets@gmail.com>
2022-02-15 15:53:09 +0200

    @RestController add packages : dao, entity, service - from com.eronryabets.mvc_hibernate_aop

---
+++ src/main/java/com/eronryabets/spring/rest/controller/MyRESTController.java
@@ -0,0 +1,7 @@
+package com.eronryabets.spring.rest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRESTController {
+}
---
+++ src/main/java/com/eronryabets/spring/rest/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.eronryabets.spring.rest.service;
+
+import com.eronryabets.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+
+}
