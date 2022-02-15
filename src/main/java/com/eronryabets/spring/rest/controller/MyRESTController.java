d238dc8b40af6c8df88c8eab158bb1a50164a044
Ihor Ryabets <eronryabets@gmail.com>
2022-02-15 20:15:41 +0200

    add Exception handle

--- src/main/java/com/eronryabets/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/eronryabets/spring/rest/controller/MyRESTController.java
@@ -1,7 +1,52 @@
 package com.eronryabets.spring.rest.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.eronryabets.spring.rest.entity.Employee;
+import com.eronryabets.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.eronryabets.spring.rest.exception_handling.NoSuchEmployeeException;
+import com.eronryabets.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class MyRESTController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees(){
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id){
+        Employee employee = employeeService.getEmployee(id);
+
+        if (employee==null){
+            throw new NoSuchEmployeeException("There is no" +
+                    "employee with ID = " + id + " in Database");
+        }
+
+        return employee;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(
+            NoSuchEmployeeException exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+
+    }
+
+
+
+
 }
